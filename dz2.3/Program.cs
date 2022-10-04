d1245986f940b67e959a48affade5edfdb8e7c70
Menua <menya2012avah@mail.ru>
2022-10-04 15:30:10 +0300

    dz2C#3

---
+++ dz2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число, от 1 до 7 ");
+string DayOfHteWeek = Console.ReadLine();
+
+if(DayOfHteWeek == "1" )
+Console.WriteLine("Рабочий день ");
+
+else if (DayOfHteWeek == "2" )
+Console.WriteLine("Рабочий день");
+
+else if (DayOfHteWeek == "3" )
+Console.WriteLine("Рабочий день");
+
+else if (DayOfHteWeek == "4" )
+Console.WriteLine("Рабочий день ");
+
+else if (DayOfHteWeek == "5" )
+Console.WriteLine("Рабочий день ");
+
+else if (DayOfHteWeek == "6" )
+Console.WriteLine("Выходной день");
+
+else if (DayOfHteWeek == "7" )
+Console.WriteLine("Выходной день");
+
+else 
+Console.WriteLine("Я ЖЕ СКАЗАЛ ОТ 1 ДО 7!!!! ");
+
